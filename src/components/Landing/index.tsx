423fefb53f2f072820e690f1dd61b20b9a891d10
Saur0w <sthapliyal085@gmail.com>
2026-05-09 18:26:29 +0530

    first commit

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import image from "next/image";
+import styles from "./style.module.scss";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import { useRef } from "react";
+
+export default function Landing() {
+    return (
+        <section className={styles.landing}>
+            
+        </section>
+    )
+}
