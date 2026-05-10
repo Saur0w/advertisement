83d5dc4315ff53f65cfad11167be9a7f9beff1c7
Saur0w <sthapliyal085@gmail.com>
2026-05-10 10:59:31 +0530

    "Main Image"

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,16 +1,30 @@
 "use client";
 
-import image from "next/image";
 import styles from "./style.module.scss";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { SplitText } from "gsap/SplitText";
 import { useRef } from "react";
+import { SplitText } from "gsap/SplitText";
+import Image from "next/image";
+
+gsap.registerPlugin(SplitText, useGSAP);
 
 export default function Landing() {
+    const bgRef = useRef<HTMLImageElement>(null);
+
+    useGSAP(() => {
+        // TODO: add parallax scroll animation with gsap
+    }, { scope: bgRef });
+
     return (
         <section className={styles.landing}>
-            
+            <Image
+                ref={bgRef}
+                src="/images/main.jpg"
+                alt="Landing background"
+                fill
+                className={styles.bgImage}
+            />
         </section>
-    )
+    );
 }
