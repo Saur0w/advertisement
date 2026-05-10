97feeb6ba3ab8d20cc3cf6c7f9ffca8c63a5190f
Saur0w <sthapliyal085@gmail.com>
2026-05-10 11:12:57 +0530

    "FeaturedWeddings"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import Landing from "@/components/Landing"
+import Landing from "@/components/Landing";
+import FeaturedWeddings from "@/components/FeaturedWeddings";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Landing />
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <Landing />
+            <FeaturedWeddings />
+        </div>
+    );
 }
