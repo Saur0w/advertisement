9f8ba8d197d558b444771578486ef7aeef63df42
Saur0w <sthapliyal085@gmail.com>
2026-05-12 10:50:52 +0530

    "Landing Page"

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -13,6 +13,8 @@ gsap.registerPlugin(SplitText, ScrollTrigger, useGSAP);
 export default function Landing() {
     const landingRef = useRef<HTMLDivElement>(null);
     const imageContainerRef = useRef<HTMLDivElement>(null);
+    const textRef = useRef<HTMLDivElement>(null);
+    const mainTextRef = useRef<HTMLHeadingElement>(null);
 
     useGSAP(() => {
         gsap.to(imageContainerRef.current, {
@@ -25,6 +27,13 @@ export default function Landing() {
                 scrub: true,
             }
         });
+
+        gsap.to(mainTextRef.current, {
+            yPercent: -50,
+            duration: 1.2,
+            delay: 5,
+            ease: "power4.inOut"
+        });
     }, { scope: landingRef });
 
     return (
@@ -36,6 +45,30 @@ export default function Landing() {
                     fill
                     priority
                 />
+            </div>
+            <div className={styles.text} ref={textRef}>
+                <div className={styles.top}>
+                    <div className={styles.top}>
+                        <div className={styles.mask}>
+                            <div className={styles.words} ref={mainTextRef}>
+                                <h1>Moments</h1>
+                                <h1>Wedding</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.left}>
+                    <h1>that</h1>
+                </div>
+                <div className={styles.right}>
+                    <h1>never</h1>
+                </div>
+                <div className={styles.bottom}>
+                    <h1>fade</h1>
+                </div>
+            </div>
+            <div className={styles.shoeImageContainer}>
+                <Image src="/images/shoe.jpg" alt="shoes" fill />
             </div>
         </section>
     );
