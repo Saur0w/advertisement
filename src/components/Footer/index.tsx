134728b5c1c06b4c37a6bfcf603952ca0e1d8760
Saur0w <sthapliyal085@gmail.com>
2026-05-14 23:25:26 +0530

    "Footer Structure"

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,10 +5,15 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
+import { useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger, SplitText, useGSAP);
 
 export default function Footer() {
+
+    useGSAP(() => {
+
+    })
     return (
         <footer className={styles.footer}>
 
